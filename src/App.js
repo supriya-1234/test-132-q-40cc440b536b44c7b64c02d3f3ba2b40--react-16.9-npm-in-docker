bf2970a82c235c4829f9528db55c9c85ea90083e
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-11-12 18:32:40 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -56,6 +56,9 @@ function App() {
       <button id="percentile" key={"%"} onClick={() => handleClick("%")}>
         %
       </button>
+      <button id="dot" key={"."} onClick={() => handleClick(".")}>
+        .
+      </button>
       <button id="equal" onClick={calculate}>
         =
       </button>
