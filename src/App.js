4350df7ef91ab742f97724cbaba31a51aee52748
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-11-12 18:29:54 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -21,18 +21,41 @@ function App() {
   const clear = function () {
     setExpression("");
   };
-  const delete1 = function () {};
+  const delete1 = function () {
+    const newExpression = expression.value;
+    newExpression = newExpression.substring(0, newExpression.length - 1);
+    setExpression(newExpression);
+
+    const copy = newExpression.filter((el) => newExpression[el !== length]);
+    setExpression(copy);
+  };
 
   return (
     <div className="App">
-      <div className="screen">{expression}</div>
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0, ".", "+", "-", "*", "/", "%"].map(
-        (el) => (
-          <button id="el" key={el} onClick={() => handleClick(el)}>
-            {el}
-          </button>
-        )
-      )}
+      <div id="result" className="screen">
+        {expression}
+      </div>
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((el) => (
+        <button id="el" key={el} onClick={() => handleClick(el)}>
+          {el}
+        </button>
+      ))}
+      <button id="add" key={"+"} onClick={() => handleClick("+")}>
+        +
+      </button>
+
+      <button id="subtract" key={"-"} onClick={() => handleClick("-")}>
+        -
+      </button>
+      <button id="multiply" key={"*"} onClick={() => handleClick("*")}>
+        *
+      </button>
+      <button id="divide" key={"/"} onClick={() => handleClick("/")}>
+        /
+      </button>
+      <button id="percentile" key={"%"} onClick={() => handleClick("%")}>
+        %
+      </button>
       <button id="equal" onClick={calculate}>
         =
       </button>
