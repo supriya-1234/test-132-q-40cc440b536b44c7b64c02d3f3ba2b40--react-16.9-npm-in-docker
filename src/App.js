556285781249284e21743cf5142bb89378fcb0c2
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-11-12 17:51:31 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { evaluate } from "mathjs";
+import "./App.css";
 
 function App() {
+  const [expression, setExpression] = useState("");
+  const calculate = function () {
+    let result;
+    try {
+      result = evaluate(expression);
+      setExpression(result);
+    } catch (ex) {
+      setExpression("");
+    }
+  };
+  const handleClick = function (value) {
+    const newExpression = expression + value;
+    setExpression(newExpression);
+  };
+
+  const clear = function () {
+    setExpression("");
+  };
+  const delete1 = function () {};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="screen">{expression}</div>
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0, ".", "+", "-", "*", "/", "%"].map(
+        (el) => (
+          <button id="el" key={el} onClick={() => handleClick(el)}>
+            {el}
+          </button>
+        )
+      )}
+      <button id="equal" onClick={calculate}>
+        =
+      </button>
+      <button id="clear" onClick={clear}>
+        AC
+      </button>
+      <button id="delete" onClick={delete1}>
+        DELETE
+      </button>
     </div>
   );
 }
